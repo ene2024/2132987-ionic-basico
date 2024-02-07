add2481a2e1ad39cab4f3ef99caee316a70fd09e
IdAle3 <>
2024-02-06 19:57:26 -0600

    Practica 1

---
+++ script.js
@@ -0,0 +1,17 @@
+var miVariable = 5; //numérico
+
+var campoTexto = document.getElementById("miTxt");
+var texto = ""; // campo.Texto.value = "";
+var borrar = "";
+
+function miFuncion(){
+    //alert(y);
+    texto = campoTexto.value;
+    alert(texto);
+}
+
+function miFuncion2(x){
+    x.value ="";
+}
+
+//miFuncion();
