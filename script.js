c3f25fd1057e315f03d8dd31d99d5fc0c0a071fc
IdAle3 <158011814+IdAle3@users.noreply.github.com>
2024-02-20 20:02:48 -0600

    Update script.js

--- script.js
+++ script.js
@@ -6,10 +6,14 @@ var monto = 0;
 
 var total = 0;
 
+const seccionTicket = document.getElementById("ticket");
+
 function agregarTicket(){
     articulo = articuloInput.ariaValueMax;
 
     monto = parseInt(precioInput.value);
 
     total = total + monto;
+
+    let elementoTicket.innerHTML =  articulo + '................ $ ' + monto;
 }
