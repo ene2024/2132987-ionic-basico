7a83954bf931bf8679ffe78fb2f7d7e938f91396
IdAle3 <idalia.delgadom@uanl.edu.mx>
2024-03-20 23:18:11 -0600

    practica 7

---
+++ miApp-2132987/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouteReuseStrategy } from '@angular/router';
+
+import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { ListaAlumnosComponent } from './lista-alumnos/lista-alumnos.component';
+
+
+
+
+@NgModule({
+  declarations: [AppComponent, ListaAlumnosComponent ],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
