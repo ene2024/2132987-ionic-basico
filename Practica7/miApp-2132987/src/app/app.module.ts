ffe1255901e0646165e1dff18aabe67c0c1de064
IdAle3 <idalia.delgadom@uanl.edu.mx>
2024-04-18 07:30:40 -0600

    Práctica 9: Servicios en Angular

--- Practica7/miApp-2132987/src/app/app.module.ts
+++ Practica7/miApp-2132987/src/app/app.module.ts
@@ -7,13 +7,15 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { ListaAlumnosComponent } from './lista-alumnos/lista-alumnos.component';
+import { ProductosComponent } from './productos/productos.component';
+import { HttpClientModule } from '@angular/common/http';
 
 
 
 
 @NgModule({
-  declarations: [AppComponent, ListaAlumnosComponent ],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  declarations: [AppComponent, ListaAlumnosComponent, ProductosComponent ],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
   bootstrap: [AppComponent],
