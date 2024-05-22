39858856d7f67c1a617a80ec6cc4b8798ae33238
IdAle3 <idalia.delgadom@uanl.edu.mx>
2024-05-22 00:00:01 -0600

    Práctica 13: Build de App

---
+++ Practica7/miApp-2132987/android/app/src/main/java/com/miApp/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.miApp.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- Practica7/miApp-2132987/capacitor.config.ts
+++ Practica7/miApp-2132987/capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'com.miApp.app',
   appName: 'miApp-2132987',
   webDir: 'www',
   server: {
